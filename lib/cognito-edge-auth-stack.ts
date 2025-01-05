536d6832f62e35bcb6bbdd780f5b97b6bda9af94
khanr <khanriza@gmail.com>
2025-01-05 16:39:43 -0500

    Moving frontend to new construct file

--- lib/cognito-edge-auth-stack.ts
+++ lib/cognito-edge-auth-stack.ts
@@ -1,40 +1,11 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import {
-  Distribution,
-  ViewerProtocolPolicy,
-} from "aws-cdk-lib/aws-cloudfront";
-import {
-  S3StaticWebsiteOrigin,
-} from "aws-cdk-lib/aws-cloudfront-origins";
-import { BlockPublicAccess, Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import path = require("path");
+import frontend from "./constructs/frontend";
 
 export class CognitoEdgeAuthStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const frontend = new Bucket(this, "Frontend", {
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
-      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-    });
-
-    const distribution = new Distribution(this, "Distribution", {
-      defaultBehavior: {
-        origin: new S3StaticWebsiteOrigin(frontend),
-        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      },
-    });
-
-    new BucketDeployment(this, "Deployment", {
-      destinationBucket: frontend,
-      sources: [Source.asset(path.resolve(__dirname, "../frontend/dist"))],
-      distribution: distribution,
-      distributionPaths: ["/*"],
-    });
+    frontend({ scope: this, name: "Frontend" });
   }
 }
---
+++ lib/constructs/frontend.ts
@@ -0,0 +1,40 @@
+import { RemovalPolicy } from "aws-cdk-lib";
+import { Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { S3StaticWebsiteOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import {
+  Bucket,
+  BlockPublicAccess,
+  BucketAccessControl,
+} from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+import path = require("path");
+
+interface Props {
+  scope: Construct;
+  name: string;
+}
+
+export default ({ scope, name }: Props) => {
+  const frontend = new Bucket(scope, `${name}Bucket`, {
+    websiteIndexDocument: "index.html",
+    publicReadAccess: true,
+    removalPolicy: RemovalPolicy.DESTROY,
+    blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+    accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+  });
+
+  const distribution = new Distribution(scope, `${name}Distribution`, {
+    defaultBehavior: {
+      origin: new S3StaticWebsiteOrigin(frontend),
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    },
+  });
+
+  new BucketDeployment(scope, `${name}Deployment`, {
+    destinationBucket: frontend,
+    sources: [Source.asset(path.resolve(__dirname, "../../frontend/dist"))],
+    distribution: distribution,
+    distributionPaths: ["/*"],
+  });
+};
