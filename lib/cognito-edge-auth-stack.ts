38d2f1374e58446ffb1d4f1e86b155493e4c4a1b
khanr <khanriza@gmail.com>
2025-01-05 16:08:46 -0500

    first pass

--- lib/cognito-edge-auth-stack.ts
+++ lib/cognito-edge-auth-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Distribution,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import {
+  S3StaticWebsiteOrigin,
+} from "aws-cdk-lib/aws-cloudfront-origins";
+import { BlockPublicAccess, Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+import path = require("path");
 
-export class CognitoEdgeAuthStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CognitoEdgeAuthStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const frontend = new Bucket(this, "Frontend", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CognitoEdgeAuthQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new S3StaticWebsiteOrigin(frontend),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    new BucketDeployment(this, "Deployment", {
+      destinationBucket: frontend,
+      sources: [Source.asset(path.resolve(__dirname, "../frontend/dist"))],
+      distribution: distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
