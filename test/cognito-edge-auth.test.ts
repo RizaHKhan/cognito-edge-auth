8f0d10f809e520edfceb86318490b5fb25af74a3
khanr <khanriza@gmail.com>
2025-01-05 10:43:39 -0500

    Initial commit

---
+++ test/cognito-edge-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CognitoEdgeAuth from '../lib/cognito-edge-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cognito-edge-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CognitoEdgeAuth.CognitoEdgeAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
