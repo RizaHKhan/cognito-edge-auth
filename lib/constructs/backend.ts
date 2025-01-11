4d6b8a1262282f59e04d1b4c4c6d0399dbcc75eb
khanr <khanriza@gmail.com>
2025-01-11 13:23:38 -0500

    updating references to function

--- lib/constructs/backend.ts
+++ lib/constructs/backend.ts
@@ -8,6 +8,7 @@ import {
 import { Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface Props {
   scope: Construct;
@@ -21,9 +22,9 @@ export default ({
   authorizerOptions,
 }: Props): { api: LambdaRestApi } => {
   const helloWorldFunction = new NodejsFunction(scope, `${name}HelloFunction`, {
-    runtime: Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
-    code: Code.fromAsset("lambda"),
-    handler: "hello.handler",
+    runtime: Runtime.NODEJS_22_X,
+    handler: "handler",
+    entry: join(__dirname, "..", "..", "lambda", "hello.ts"),
   });
 
   const api = new RestApi(scope, `${name}AG`, {
@@ -32,42 +33,14 @@ export default ({
     },
   });
 
-  api.root.addCorsPreflight({
-    allowOrigins: Cors.ALL_ORIGINS,
-    allowMethods: Cors.ALL_METHODS, // This will allow all methods (GET, POST, etc.)
-  });
-
   const helloResource = api.root.addResource("hello");
-  helloResource.addMethod("GET", new LambdaIntegration(helloWorldFunction), {
-    ...authorizerOptions,
-    methodResponses: [
-      {
-        statusCode: "200",
-        responseParameters: {
-          "method.response.header.Access-Control-Allow-Origin": true,
-          "method.response.header.Access-Control-Allow-Headers": true,
-          "method.response.header.Access-Control-Allow-Methods": true,
-        },
-      },
-    ],
-  });
-
   helloResource.addMethod(
-    "OPTIONS",
+    "GET",
     new LambdaIntegration(helloWorldFunction),
-    {
-      methodResponses: [
-        {
-          statusCode: "200",
-          responseParameters: {
-            "method.response.header.Access-Control-Allow-Origin": true,
-            "method.response.header.Access-Control-Allow-Headers": true,
-            "method.response.header.Access-Control-Allow-Methods": true,
-          },
-        },
-      ],
-    },
+    authorizerOptions,
   );
+
+  helloResource.addMethod("OPTIONS", new LambdaIntegration(helloWorldFunction));
 
   return { api };
 };
